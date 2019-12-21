af7d45e70b9ab6dbe97e1e57617dea713dd37a10
Arnaud Coquelet <arcoquel@gmail.com>
2019-12-20 22:14:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@
 import uuid
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 __author__ = 'Fernando Garcia <fernando@cutre.net>'
 
